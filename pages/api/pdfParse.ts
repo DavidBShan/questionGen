71914eca6a110440a84071d042fa70bff9e84b3e
David Shan <davidbshan@gmail.com>
2023-10-13 00:33:42 -0400

    tessting

--- pages/api/pdfParse.ts
+++ pages/api/pdfParse.ts
@@ -1,6 +1,7 @@
 import formidable from 'formidable';
-const fs = require('fs');
-const pdf = require('pdf-parse');
+import { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import pdf from 'pdf-parse';
 
 export const config = {
   api: {
@@ -8,41 +9,47 @@ export const config = {
   },
 };
 
-export default function uploadPDFHandler(req: any, res: any) {
-  const form = new formidable.IncomingForm();
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const form = new formidable.IncomingForm();
 
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      console.error('Error:', err.message);
-      res.status(500).json({ error: 'An error occurred during processing' });
-      return;
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error('Error:', err.message);
+        res.status(500).json({ error: 'An error occurred during processing' });
+        return;
+      }
+
+      const pdfFile = files.pdf;
+
+      if (!pdfFile) {
+        res.status(400).json({ error: 'PDF file not found in formData' });
+        return;
+      }
+      console.log('pdfFile', pdfFile);
+
+      try {
+        //const txt = await readPDF(pdfPath);
+        res.status(200).json({ text: pdfFile});
+      } catch (error) {
+        console.error('Error processing PDF:', error);
+        res.status(500).json({ error: 'An error occurred while processing the PDF.' });
+      }
+    });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
+
+async function readPDF(pdfPath: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const dataBuffer = fs.readFileSync(pdfPath);
+      pdf(dataBuffer).then(function (data: any) {
+        resolve(data.text);
+      });
+    } catch (error) {
+      reject(error);
     }
-    if(files.pdf) {
-        const pdfFile = files.pdf[0];
-        if (!pdfFile) {
-            res.status(400).json({ error: 'PDF file not found in formData' });
-            return;
-        }
-      
-          const pdfPath = (pdfFile as any).path;
-      
-          try {
-            const dataBuffer = fs.readFileSync(pdfPath);
-      
-            pdf(dataBuffer)
-              .then(function (data: any) {
-                const text = data.text;
-      
-                // Save the text to a JSON file
-                const jsonData = { text };
-                res.status(200).json({ text });
-              })
-              .catch(function (error: any) {
-                res.status(500).json({ error: 'An error occurred while processing the PDF.' });
-              });
-          } catch (error) {
-            res.status(500).json({ error: 'An error occurred while processing the PDF.' });
-          }
-    } 
   });
 }
