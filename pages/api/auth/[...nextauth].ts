dea43136df2aff1ae3bc1cb33b617d2b3862d461
David Shan <davidbshan@gmail.com>
2023-10-12 23:52:31 -0400

    move pages back

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import { auth } from "@/app/firebase/config";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  pages: {
+    signIn: '/login'
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {},
+      async authorize(credentials): Promise<any> {
+        return await signInWithEmailAndPassword(auth, (credentials as any).email || '', (credentials as any).password || '')
+          .then(userCredential => {
+            if (userCredential.user) {
+              return userCredential.user;
+            }
+            return false;
+          })
+          .catch(error => (console.log(error)))
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+      }
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+  
+  secret: process.env.NEXTAUTH_SECRET,
+
+}
+export default NextAuth(authOptions)
---
+++ pages/api/uploadPDF.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+const formidable = require('formidable');
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default function handler(req:any, res:any) {
+  try {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = path.join(process.cwd(), 'uploads'); 
+    form.parse(req, async (err:any, fields:any, files:any) => {
+      if (err) {
+        throw new Error('Form parsing error: ' + err.message);
+      }
+      const pdfFile = files.pdf;
+      if (!pdfFile) {
+        throw new Error('PDF file not found in formData');
+      }
+      const uploadsFolder = path.join(process.cwd(), 'uploads');
+      if (!fs.existsSync(uploadsFolder)) {
+        fs.mkdirSync(uploadsFolder);
+      }
+      const uniqueFileName = `uploadedfile`;
+      fs.renameSync(pdfFile[0].filepath, path.join(uploadsFolder, uniqueFileName));
+
+      res.status(200).json({ message: 'PDF uploaded and saved successfully' });
+    });
+  } catch (error:any) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'An error occurred during processing' });
+  }
+}
