43c7c9d07db05464ef41db9e45565fffcae0cb61
santiagu-gap <santiagu.gap@gmail.com>
2023-10-23 22:51:01 -0400

    New navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+// import UserMenu from './authentification/UserMenu';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <div className="fixed w-full bg-white">
-        <nav className="flex items-center justify-between border-y-4 border-blue-200 p-6 md:px-24">
+    <div className="w-full bg-white">
+        <nav className="flex items-center justify-between border-y-4 border-blue-200 p-6 md:px-16">
         <div className='absolute opacity-0 md:relative md:opacity-100'>
             <span className="text-sm md:text-lg">Practice Questions</span>
         </div>
@@ -11,9 +15,40 @@ const Navbar = () => {
         <div className="text-2xl font-black md:text-4xl">
             <span>Aceflow</span>
         </div>
+
+        <div className='flex flex-row gap-2 md:flex-col'>
+            <button onClick={()=>{router.push(`/pro`)}} 
+            className="w-full rounded-xl bg-aceflow-blue p-1 text-sm font-medium text-white hover:bg-blue-600 md:p-3 md:px-5 md:text-lg">
+            Try Aceflow Pro+
+            </button>
+            {/* <UserMenu/> */}
+        </div>
+
+        
+
         </nav>
     </div>
   );
 };
 
 export default Navbar;
+
+// import React from 'react';
+
+// const Navbar = () => {
+//   return (
+//     <div className="fixed w-full bg-white">
+//         <nav className="flex items-center justify-between border-y-4 border-blue-200 p-6 md:px-24">
+//         <div className='absolute opacity-0 md:relative md:opacity-100'>
+//             <span className="text-sm md:text-lg">Practice Questions</span>
+//         </div>
+
+//         <div className="text-2xl font-black md:text-4xl">
+//             <span>Aceflow</span>
+//         </div>
+//         </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
