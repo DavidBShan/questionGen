bd22d9bb538daa0980ab750b04c252f7c2f62c32
santiagu-gap <santiagu.gap@gmail.com>
2023-10-26 22:27:58 -0400

    Updated the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,19 +8,23 @@ const Navbar = () => {
   return (
     <div className="w-full bg-white">
         <nav className="flex items-center justify-between border-y-4 border-blue-200 p-6 md:px-16">
-        <div className='absolute opacity-0 md:relative md:opacity-100'>
+        {/* <div className='absolute opacity-0 md:relative md:opacity-100'>
             <span className="text-sm md:text-lg">Practice Questions</span>
-        </div>
+        </div> */}
 
+        <div>
+        <button onClick={()=>{router.push(`/pro`)}} 
+            className="w-full rounded-xl bg-aceflow-blue p-3 text-sm font-medium text-white hover:bg-blue-600 md:px-5 md:text-lg">
+            Try Aceflow Pro+
+        </button>
+
+        </div>
+       
         <div className="text-2xl font-black md:text-4xl">
             <span>Aceflow</span>
         </div>
 
         <div className='flex flex-row gap-2 md:flex-col'>
-            <button onClick={()=>{router.push(`/pro`)}} 
-            className="w-full rounded-xl bg-aceflow-blue p-1 text-sm font-medium text-white hover:bg-blue-600 md:p-3 md:px-5 md:text-lg">
-            Try Aceflow Pro+
-            </button>
             <UserMenu/>
         </div>
 
